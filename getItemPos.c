d2bb15abac7bc0260f20d749a7790bef4274e9c5
Anish Ghai <anishgdev@gmail.com>
2018-05-31 15:44:27 +1000

    Create getItemPos.c

---
+++ getItemPos.c
@@ -0,0 +1,7 @@
+float* getItemPos(int itemID) {
+  float itemPos[3]; 
+  // Compute item position. Return pointer to the array. A pointer is the memory address of the float. 
+  // The return value will be computed by taking the pointer and adding until we reach the last element of the array. 
+  
+  return itemPos;
+}
