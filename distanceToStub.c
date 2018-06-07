5960fd615146831a5becb61327369c2b6e1a6ff6
Anish Ghai <anishgdev@gmail.com>
2018-06-08 00:41:55 +1000

    Updated distanceToStub to return float values
    Updated distanceToStub to work properly and return float values

--- distanceToStub.c
+++ distanceToStub.c
@@ -1,6 +1,6 @@
 float distanceToStub(float place[3], float dest[3]){
   float vecBetween[3];
   mathVecSubtract(vecBetween, dest, place, 3);
-  mathVecMagnitude(vecBetween, 3);
-  return vecBetween
+  distance = mathVecMagnitude(vecBetween, 3);
+  return distance;
 }
